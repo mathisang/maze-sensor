d32a1e4ca06cba5b841de1f36ae1c4451ef2d0cb
mathisangibaud <m.angibaudmathis@gmail.com>
2023-05-04 09:53:42 +0200

    maze import & rotation

--- src/script.js
+++ src/script.js
@@ -1,28 +1,56 @@
 import './style.css'
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {GUI} from 'dat.gui'
+
+class DataPoint {
+    constructor() {
+        this.arr = []
+    }
+
+    setFilter(n) {
+        this.sum = 0;
+
+        if(this.arr.length >= n) {
+            for(this.i = 0; this.i < n; this.i++) {
+                this.sum += this.arr[this.arr.length - (this.i+1)]
+            }
+        }
+
+        return this.sum / n;
+    }
+
+    push(data) {
+        this.arr.push(data)
+    }
+
+    getDataFilter() {
+        return this.setFilter(3)
+    }
+}
+
+let moveX = new DataPoint();
+let moveY = new DataPoint();
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-let moveX = 0;
-let moveY = 0;
-
+// Moouse & Mobile orientation
 if (window.DeviceOrientationEvent) {
     window.addEventListener(
         "deviceorientation",
         (event) => {
-            moveX = event.gamma;
-            moveY = event.beta;
+            moveX.push(event.gamma)
+            moveY.push(event.beta)
         },
         true
     );
 }
 else if (window.matchMedia('(hover: hover)').matches) {
     window.addEventListener('mousemove', (event) => {
-        moveX = (event.clientX / sizes.width) * 60 - 30;
-        moveY = (event.clientY / sizes.height) * 60 - 30;
+        moveX.push((event.clientX / sizes.width) * 60 - 30);
+        moveY.push((event.clientY / sizes.height) * 60 - 30);
     });
 }
 
@@ -67,44 +95,53 @@ scene.add(camera)
 const controls = new OrbitControls(camera, canvas)
 
 // Load a resource
-// const loader = new OBJLoader();
-// loader.load(
-//     // resource URL
-//     '3dmodels/maze.gltf',
-//     // called when resource is loaded
-//     function ( object ) {
-//         scene.add( object );
-//     },
-//     function (xhr) {
-//         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-//     },
-//     function (error) {
-//         console.log('An error happened');
-//     }
-// );
+const loader = new GLTFLoader();
+let maze;
+loader.load(
+    '/models/scene.gltf',
+    (gltf) =>
+    {
+        maze = gltf.scene
+        maze.scale.set(0.035, 0.035, 0.035)
+        maze.rotation.x = Math.PI / 2
+        scene.add(maze)
+    }
+)
+
+// console.log(gltf)
 
 // Cube
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1.5, 2, .4),
-    new THREE.MeshStandardMaterial({
-        color: '#ffffff'
-    })
-)
-cube.castShadow = true
-cube.receiveShadow = true
-scene.add(cube)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1.5, 2, .4),
+//     new THREE.MeshStandardMaterial({
+//         color: '#ffffff'
+//     })
+// )
+// cube.castShadow = true
+// cube.receiveShadow = true
+// scene.add(cube)
 
 // Ambient Light
-const light = new THREE.AmbientLight("#deaaaa");
-light.intensity = .3;
+const light = new THREE.AmbientLight("#aadebf");
+light.intensity = .43;
 scene.add(light);
 
 // Directional Light
-const directionalLight = new THREE.DirectionalLight('#ffffff', .7)
-directionalLight.position.x = 2
-directionalLight.position.y = 5
-directionalLight.position.z = 3
+const directionalLight = new THREE.DirectionalLight('#ffffff', .87)
+directionalLight.position.x = 1
+directionalLight.position.y = 2
+directionalLight.position.z = 5.5
 scene.add(directionalLight)
+
+// GUI
+// const gui = new GUI()
+// const lightFolder = gui.addFolder('Lights')
+// lightFolder.add(directionalLight.position, 'x').min(-20).max(20).step(.5)
+// lightFolder.add(directionalLight.position, 'y').min(-20).max(20).step(.5)
+// lightFolder.add(directionalLight.position, 'z').min(-20).max(20).step(.5)
+// lightFolder.add(directionalLight, 'intensity').min(0).max(2).step(.01)
+// lightFolder.add(light, 'intensity').min(0).max(2).step(.01)
+// lightFolder.open()
 
 // Animate
 const clock = new THREE.Clock()
@@ -117,11 +154,13 @@ const tick = () => {
 
     // Update controls
     // controls.update()
-    // cube.rotation.x = moveY / 100
-    // cube.rotation.y = moveZ / 100
 
-    cube.rotation.x = moveY / 100
-    cube.rotation.y = moveX / 100
+    // cube.rotation.x = moveY.getDataFilter() / 100
+    // cube.rotation.y = moveX.getDataFilter() / 100
+    if(maze) {
+        maze.rotation.x = (Math.PI / 2) + (moveY.getDataFilter() / 100)
+        maze.rotation.z = -(moveX.getDataFilter() / 100)
+    }
 
     // Render
     renderer.render(scene, camera)
